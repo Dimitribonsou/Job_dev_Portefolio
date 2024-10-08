921bf22403e93db906ad13f9b1dc06cb532ecd6c
Dimitribonsou <dimitribonsou26@gmail.com>
2024-10-08 09:37:29 +0100

    responsive de la section service corriger

--- src/Components/Sous-Components/ServicesItem.tsx
+++ src/Components/Sous-Components/ServicesItem.tsx
@@ -10,8 +10,8 @@ const ServicesItem = (props:servicetype) => {
       <div className="bg-green w-16 h-16 rounded-full absolute -top-6 left-1/2 -translate-x-1/3 -translate-y-2 flex justify-center items-center">
          <img src={croix} alt="croix" className="w-1/2"></img>
       </div>
-      <span className="text-xl font-bold mt-7 text-green mb-3">{props.title}</span>
-      <p className="text-justify text-inherit md:text-x  md:text-sm  xl:text-base font-medium">
+      <span className="service-title font-bold mt-7 text-green mb-3">{props.title}</span>
+      <p className="text-justify service-desc md:text-x  md:text-sm  xl:text-base font-medium">
         {props.description}
       </p>
       <div className="flex justify-end items-center   w-full">
