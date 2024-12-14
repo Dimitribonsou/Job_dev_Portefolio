d476bfe66634a81b5d5ce2a03a076d2da4ae862a
Dimitribonsou <dimitribonsou26@gmail.com>
2024-12-14 05:25:25 +0100

    ajout du script de google analytics dans le projet

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,18 @@ function App() {
                   <meta property="og:description" content="Développeur web et mobile à Douala, Cameroun. Spécialisé dans le développement de solutions web personnalisées et d'applications mobiles pour les entreprises." />
                   <meta property="og:url" content="https://dimitribonsou-website.vercel.app/" />
                   <meta property="og:image" content={logo} />
+                  {/* ajouter la balise de verification de google */}
                   <meta name="google-site-verification" content="C5ftg5VXj5bdp7FA8aBYQZt-xwKjbTg4xNj8wQyzUZM" />
+                  {/* <!-- Google tag (gtag.js) --> */}
+                  <script async src="https://www.googletagmanager.com/gtag/js?id=G-WTEP5953F2"></script>
+                  <script>
+                    {`
+                      window.dataLayer = window.dataLayer || [];
+                      function gtag(){dataLayer.push(arguments);}
+                      gtag('js', new Date());
+                      gtag('config', 'G-WTEP5953F2');
+                    `}
+                  </script>
         </Helmet>
        {/* appel des differents composants */}
        <Navbar/>
